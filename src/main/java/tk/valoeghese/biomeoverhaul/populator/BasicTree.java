069f38d99fdf4355a249c06c5c93c972e549d22e
valoeghese <valoeghesemods@gmail.com>
2020-03-24 16:18:27 +1300

    e

--- src/main/java/tk/valoeghese/biomeoverhaul/populator/BasicTree.java
+++ src/main/java/tk/valoeghese/biomeoverhaul/populator/BasicTree.java
@@ -2,6 +2,9 @@ package tk.valoeghese.biomeoverhaul.populator;
 
 import java.util.Random;
 
+import net.minecraft.block.FluidBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.IWorld;
 import tk.valoeghese.worldcomet.api.populator.SurfacePopulator;
@@ -13,6 +16,27 @@ public abstract class BasicTree extends SurfacePopulator {
 		this.heightSample = world.getChunk(chunkX, chunkZ).sampleHeightmap(Heightmap.Type.WORLD_SURFACE_WG, 0, 0);
 		super.populateChunk(world, rand, chunkX, chunkZ, surfaceProvider, seed);
 	}
+
+	@Override
+	protected boolean generate(IWorld world, Random rand, SurfaceProvider surfaceProvider, int x, int y, int z) {
+		BlockPos.Mutable pos = new BlockPos.Mutable(x, y, z);
+
+		if (world.getBlockState(pos.down()).getBlock() instanceof FluidBlock) {
+			return false;
+		}
+
+		int height = this.calculateHeight(world, rand, x, z);
+
+		if (this.canGenerate(world, x, y, z, height)) {
+			return this.generate(world, rand, surfaceProvider, x, y, z, height);
+		} else {
+			return false;
+		}
+	}
+
+	protected abstract int calculateHeight(BlockView world, Random rand, int x, int z);
+	protected abstract boolean canGenerate(BlockView world, int x, int y, int z, int height);
+	protected abstract boolean generate(IWorld world, Random rand, SurfaceProvider surfaceProvider, int x, int y, int z, int height);
 
 	protected int heightSample = 63;
 }
--- src/main/java/tk/valoeghese/biomeoverhaul/populator/normal/PineTree.java
+++ src/main/java/tk/valoeghese/biomeoverhaul/populator/normal/PineTree.java
@@ -3,7 +3,9 @@ package tk.valoeghese.biomeoverhaul.populator.normal;
 import java.util.Random;
 
 import net.minecraft.block.Blocks;
+import net.minecraft.block.FluidBlock;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.IWorld;
 import tk.valoeghese.biomeoverhaul.populator.BasicTree;
 import tk.valoeghese.worldcomet.api.surface.SurfaceProvider;
@@ -11,23 +13,35 @@ import tk.valoeghese.worldcomet.api.surface.SurfaceProvider;
 public class PineTree extends BasicTree {
 	@Override
 	protected int getCount(Random rand, SurfaceProvider surfaceProvider, int chunkX, int chunkZ) {
-		double temperature = NormalPopulatorMaths.INSTANCE.getTemperature(chunkX, chunkZ);
+		//double temperature = NormalPopulatorMaths.INSTANCE.getTemperature(chunkX, chunkZ);
 
 		//if (temperature > 0.6) {
-			// [0,2]
-			double humidity = NormalPopulatorMaths.INSTANCE.getHumidity(chunkX, chunkZ) + 1.0;
+		// [0,2]
+		double humidity = NormalPopulatorMaths.INSTANCE.getHumidity(chunkX, chunkZ) + 1.0;
 
-			int countModifier = NormalPopulatorMaths.INSTANCE.getCountModifier(surfaceProvider.getSurface(chunkX << 4, chunkZ << 4, this.heightSample));
-	
-			// 3 * countModifier at average humidity
-			return (int) (3 * countModifier * humidity);
+		int countModifier = NormalPopulatorMaths.INSTANCE.getCountModifier(surfaceProvider.getSurface(chunkX << 4, chunkZ << 4, this.heightSample));
+
+		// 3 * countModifier at average humidity
+		return (int) (3 * countModifier * humidity);
 		//}
 	}
 
 	@Override
-	protected boolean generate(IWorld world, Random rand, SurfaceProvider surfaceProvider, int x, int y, int z) {
-		BlockPos.Mutable pos = new BlockPos.Mutable(x, y, z);
-		setBlockState(world, pos, Blocks.BEDROCK.getDefaultState());
-		return true;
+	protected int calculateHeight(BlockView world, Random rand, int x, int z) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected boolean canGenerate(BlockView world, int x, int y, int z, int height) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean generate(IWorld world, Random rand, SurfaceProvider surfaceProvider, int x, int y, int z,
+			int height) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
